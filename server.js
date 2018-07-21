3b91e6f1d9e6a04cf52c920f687eda425039babe
Francisca Vargas <francisca@franciscavargas.me>
2018-07-21 13:14:03 +0100

    Express Formidable

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require("express");
+// Express Formidable library
+var formidable = require("express-formidable");
 var app = express();
+app.use(formidable());
 
 // Built-in middleware function that comes with Express. Serve all the static express
 // in our public folder.
 app.use(express.static("public"));
+
+// Define a route to deal with a POST request
+// app.post("/create-post", function(req, res) {
+//   console.log("hello");
+// });
 
 // App listen takes 2 parameters: a port and a callback argument
 app.listen(3000, function() {
