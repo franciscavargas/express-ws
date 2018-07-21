ef331582c0df01f6fb6796c1044a3474f8855418
Francisca Vargas <francisca@franciscavargas.me>
2018-07-21 12:41:58 +0100

    Middleware function to serve public

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
 var express = require("express");
 var app = express();
 
-// Handle Function -> method is used to define a handler function in Express.
-// It takes two parameters: the endpoint at which to trigger an action and the
-// handler function that tells it exactly what to do.
-
-app.get("/", function(req, res) {
-  res.send("Yay Node Girls!");
-});
-
-//Create multiple endpoints and send different responses
-app.get("/chocolate", function(req, res) {
-  res.send("Mm chocolate :O");
-});
-
-app.get("/node", function(req, res) {
-  res.send("Hujuu");
-});
-
-app.get("/girls", function(req, res) {
-  res.send("girls");
-});
+// Built-in middleware function that comes with Express. Serve all the static express
+// in our public folder.
+app.use(express.static("public"));
 
 // App listen takes 2 parameters: a port and a callback argument
 app.listen(3000, function() {
